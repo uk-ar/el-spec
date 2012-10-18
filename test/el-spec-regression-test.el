1dbc465401d4aea5560318c4f13ff30920a0718d
Joel McCracken <mccracken.joel@gmail.com>
2012-10-18 03:04:03 -0400

    application now supports nested describes.

--- test/el-spec-regression-test.el
+++ test/el-spec-regression-test.el
@@ -13,20 +13,20 @@
         (it "outer-it"
           'nothing-here)
         
-          (describe "inner"
-            (it "inner-it"
-              'nothing-here-either)
-            
-            (describe "intestines"
-              (it "intestines-it"
-                'nothing-here-again))))
-        
-      (dolist (tst tests)
-        (ert-run-test (ert-get-test tst)))
-      
-      (should (ert-test-boundp (intern "outer\nouter-it")))
-      (should (ert-test-boundp (intern "outer\ninner\ninner-it")))
-      
+        (describe "inner"
+          (it "inner-it"
+            'nothing-here-either)
+          
+          (describe "intestines"
+            (it "intestines-it"
+              'nothing-here-again))))
+
+
+      (let ((tests (list (intern "outer\nouter-it")
+                         (intern "outer\ninner\ninner-it"))))
+        (dolist (tst tests)
+          (ert-test-boundp tst)
+          (ert-run-test (ert-get-test tst))))
       (should (= num-calls 2)))))
 
 (ert-deftest el-spec:contexts-should-also-do-what-we-want  ()
@@ -40,5 +40,5 @@
       (it "aba")))
   
   (should (ert-test-boundp (intern "wrap\nouter context\nx")))
-  (should (ert-test-boundp (intern "wrap\nouter context\minner context\ny")))
+  (should (ert-test-boundp (intern "wrap\nouter context\ninner context\ny")))
   )
